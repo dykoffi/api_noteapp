956ea75c09af56cff06e14383b907fe889e9f0e0
dykoffi <dykoffi2000@gmail.com>
2021-02-24 19:22:32 +0000

    change files struture in database

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # api_noteapp
+
+sudo -iu postgres
+psql
+
+>CREATE ROLE oscav WITH LOGIN PASSWORD '1234';
+>CREATE DATABASE noteapp owner oscav;
+>exit
+
+cd databases/
+psql noteapp -U oscav -h localhost
+
+>\i db.sql
+>\i insert.sql
+>exit
+
+cd ..
+node index.js
--- index.js
+++ index.js
@@ -18,17 +18,28 @@ function normalizePort(val) {
     if (port >= 0) { return port; }
     return false;
 }
+
 function onError(error) {
     if (error.syscall !== 'listen') { throw error; }
     const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
     switch (error.code) {
-        case 'EACCES': console.error(bind + ' requires elevated privileges'); process.exit(1); break; case 'EADDRINUSE': console.error(bind + ' is already in use'); process.exit(1); break; default: throw error;
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
     }
 }
 
 function onListening() {
     const addr = server.address();
-    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port; debug('Listening on ' + bind);
+    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    debug('Listening on ' + bind);
 }
 
 console.log(`API start on port : ${port}`);
